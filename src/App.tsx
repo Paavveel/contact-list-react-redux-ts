4006c2aed7fd376b60c463d62987719303de2b39
Paavveel <pa.tomilin@gmail.com>
2022-10-04 17:56:52 +0300

    refactor: add routes helper

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { AppRoutes } from './helpers/routes';
 import { Contacts, Login } from './pages';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path='/' element={<Login />} />
-      <Route path='contacts' element={<Contacts />} />
+      <Route path={AppRoutes.HOME} element={<Login />} />
+      <Route path={AppRoutes.CONTACTS} element={<Contacts />} />
     </Route>
   )
 );
