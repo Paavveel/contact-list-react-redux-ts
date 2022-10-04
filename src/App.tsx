62f9e67e08c0cabe923dfbdee4b8038b09ad3c98
Paavveel <pa.tomilin@gmail.com>
2022-10-04 15:52:04 +0300

    feat: add main routing with RRD v6.4 draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import { Contacts } from './pages/Contacts';
 import { Login } from './pages/Login';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Login />} />
+      <Route path='contacts' element={<Contacts />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <div className='App'>
-      <Login />
-      <Contacts />
+      <RouterProvider router={router} />
     </div>
   );
 }
